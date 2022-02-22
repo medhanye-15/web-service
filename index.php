f85333bdeac90b2e3b18a2fd4cb205b233e790ef
Medhanye Weldegebriel <medhanyeleake@gmail.com>
2022-02-22 00:25:08 -0800

    Ads repl.it configuration files and index

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+     <title></title>
+</head  
+<body>
+    <h1>PHP Page!<h/1>
+    <p>This is HTML</p>
+    <?php
+        echo"<p>This is PHP</p>";
+    <p>This is HTML</p>
+    ?>
+</body>
+
+
+
+
+</html>
