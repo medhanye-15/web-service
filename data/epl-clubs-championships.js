df0ba3beea580c8aaed66e962c027722e471247d
Medhanye Weldegebriel <91928287+medhanye-15@users.noreply.github.com>
2022-03-06 19:41:03 -0800

    Create epl-clubs-championships.js

---
+++ data/epl-clubs-championships.js
@@ -0,0 +1,85 @@
+{
+	"title": "NBA Teams By Championships",
+	"teams":[
+        {
+				"Name":"Lakers",
+				"Location":"Los Angeles",
+				"Division":"Pacific",
+        "Championships":"17",
+				"LastChip":"2020",
+				"Image":"lakers.jpg"
+			  },
+        {
+				"Name":"Golden State Warriors",
+				"Location":"San Francisco",
+				"Division":"Pacific",
+        "Championships":"6",
+				"LastChip":"2018",
+				"Image":"gsw.jpg"
+			  },
+        {
+				"Name":"Bulls",
+				"Location":"Chicago",
+				"Division":"Central",
+        "Championships":"6",
+				"LastChip":"1998",
+				"Image":"bulls.jpg"
+			  },
+        {
+				"Name":"Pistons",
+				"Location":"Detroit",
+				"Division":"Central",
+        "Championships":"3",
+				"LastChip":"2004",
+				"Image":"pistons.jpg"
+			  },
+        {
+				"Name":"Pacers",
+				"Location":"Indiana",
+				"Division":"Central",
+        "Championships":"3",
+				"LastChip":"1973",
+				"Image":"pacers.jpg"
+			  },
+        {
+				"Name":"Bucks",
+				"Location":"Milwaukee",
+				"Division":"Central",
+        "Championships":"2",
+				"LastChip":"2021",
+				"Image":"bucks.jpg"
+			  },
+        {
+				"Name":"Cavaliers",
+				"Location":"Cleveland",
+				"Division":"Central",
+        "Championships":"1",
+				"LastChip":"2016",
+				"Image":"cavs.jpg"
+			  },
+        {
+				"Name":"Kings",
+				"Location":"Sacramento",
+				"Division":"Pacific",
+        "Championships":"1",
+				"LastChip":"1951",
+				"Image":"kings.jpg"
+			  },
+        {
+				"Name":"Clippers",
+				"Location":"Los Angeles",
+				"Division":"Pacific",
+        "Championships":"0",
+				"LastChip":"N/A",
+				"Image":"clippers.jpg"
+			  },
+        {
+				"Name":"Suns",
+				"Location":"Phoenix",
+				"Division":"Pacific",
+        "Championships":"0",
+				"LastChip":"N/A",
+				"Image":"suns.jpg"
+			  }
+			]
+}
