6fec9c930912d25c15cbf67c9c81e295ac2fd091
Medhanye Weldegebriel <91928287+medhanye-15@users.noreply.github.com>
2022-03-09 07:30:19 +0000

    added files

---
+++ data/bond-box-office.js
@@ -0,0 +1,126 @@
+{
+	"title": "Bond Films by Box Office Totals",
+	
+	"films":[
+            {
+                "Film":1,
+                "Title":"Avatar",
+                "Year":2009,
+                "Director":"James Cameron",
+                "Producers":"James Cameron and Jon Landau",
+                "Writers":"James Camern",
+                "Composer": "James Horner",
+                "Budget":"$237,000,000",
+                "BoxOffice":"$2.847 Billion(United States)",
+                "Image":"a-view-to-a-kill.jpg"
+              },
+              {
+                "Film":3,
+                "Title":"Black Panther",
+                "Year":2018,
+                "Director":"Ryan Coogler",
+                "Producers":"Kevin Feige",
+                "Writers":"Ryan coogler and Joe Robert Cole",
+                "Composer": "Ludwig Goransson",
+                "Budget":"$200,000,000",
+                "BoxOffice":"$1.348 Billion(United States)",
+                "Image":""   
+              },
+              {
+                "Film":5,
+                "Title":"Iron Man 3",
+                "Year":2013,
+                "Director":"Shane Black",
+                "Producers":"Kvin Feige",
+                "Writers":"Shane Black abd Stan Lee and Jack Kirby and Drew Pearce and Larry Leiber",
+                "Composer": "Brain Tayler",
+                "Budget":"$200,000,000",
+                "BoxOffice":"$1.214 Billion(United States)",
+                "Image":""                   
+              },
+              {
+                "Film":10,
+                "Title":"Aladdin",
+                "Year":2019,
+                "Director":"Guy Ritchie",
+                "Producers":"Dan Lin and Jonathan Eirich",
+                "Writers":"Jhon August and Guy Ritchie",
+                "Composer":"Alan Menken",
+                "Budget":"$183,000,000",
+                "BoxOffice":"$1.051 Billion(United States)",
+                "Image":""
+              },
+              {
+                "Film":8,
+                "Title":"Hancock",
+                "Year":2008,
+                "Director":"Peter Berg",
+                "Producers":"Akiva Goldsman and James Lassiter and Michael Mann and Will Smith ",
+                "Writers":"VY Vincent Ngo and Vince Gilligan",
+                "Composer":"Jhon Powell",
+                "Budget":"$150,000,000",
+                "Image":"$629.4 Million"
+              },
+              {
+                "Film":4,
+                "Title":"The Carate Kid",
+                "Year":2010,
+                "Director":"Christopher Murphey",
+                "Producers":"Will Smith and Jada Pinkett Smith and Jerry Weintraub",
+                "Writers":"Robert Mark Kamen",
+                "Composer": "James Horner",
+                "Budget":"$40,000,000",
+                "BoxOffice":"$359.1 Million(United States)",
+                "Image":""    
+              },
+              {
+                "Film":6,
+                "Title":"Capitain America:The First Avenger",
+                "Year":2011,
+                "Director":"Joe Johnston",
+                "Producers":"Kevin Feige",
+                "Writers":"Jack Kirby and Joe Simon",
+                "Composer": "Alan Silvestri",
+                "Budget":"$140-216.7 Million",
+                "BoxOffice":"$307.6 Million(United States)",
+                "Image":""   
+                
+              },
+              {
+                "Film":9,
+                "Title":"Unbreakable",
+                "Year":2000,
+                "Director":"M.Night Shyamalan",
+                "Producers":"Barry Mendel and Sam Mercer and M.Night Shyamalan ",
+                "Writers":"M.Night Shyamalan",
+                "Composer": "James Newton Howard",
+                "Budget":"$75,000,000",
+                "BoxOffice":"$248.1 Million(United States)",
+                "Image":""   
+              },
+              {
+                "Film":2,
+                "Title":"Blood Diamond",
+                "Year":2006,
+                "Director":"Edward Zwick",
+                "Producers":"Marshall Herskovitz, Grham King,                     Paula Weinstein, Alexander Breault-Boucher",
+                "Writers":"Charles Leavitt",
+                "Composer": "James Newton Howard",
+                "Budget":"$100,000,000",
+                "BoxOffice":"$171.7 Million(United States)",
+                "Image":""              
+              },
+              {
+                "Film":10,
+                "Title":"King Richard",
+                "Year":2021,
+                "Director":"Reinaldo Marcus Green",
+                "Producers":"Tim White and Trever White and Will smith",
+                "Writers":"Zach Baylin",
+                "Composer": "Kris Bowers",
+                "Budget":"$50,000,000",
+                "BoxOffice":"$36.3 Million(United States)",
+                "Image":""   
+    			    }
+			    ]
+}
