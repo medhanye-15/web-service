1264417a743937c1a5e6e3a0f1f144a58efe6211
Medhanye Weldegebriel <91928287+medhanye-15@users.noreply.github.com>
2022-03-09 03:14:33 +0000

    added picture

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html>
 <head>
-<title>EPL Teams Web service Demo</title>
+<title>Bond Web Service Demo</title>
 <style>
-  body {font-family:georgia;}
-  .team{
+body {font-family:georgia;}
+    .film{
     border:1px solid #E77DC2;
     border-radius: 5px;
     padding: 5px;
     margin-bottom:5px;
-    position:relative;   
+    position:relative;  
   }
  
   .pic{
@@ -16,84 +16,79 @@
     right:10px;
     top:10px;
   }
-  .pic img{
-    max-width:70px;
+.pic img{
+max-width:100px;
   }
 </style>
 <script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
 <script type="text/javascript">
-function eplTemplate(team){
-  return `<div class="team">
-      <b>Title: <b> ${team.Name}<br />
-      <b>Location: <b> ${team.Location}<br />
-      <b>Division: <b> ${team.Division}<br />
-      <b>Championships: <b> ${team.Championships}<br />
-      <b>Last Championship: <b> ${team.LastChip}<br />
-      <div class="pic"><img src="thumbnails/${team.Image}"/></div>
-    </div>`;
-}
-  
+
+ function bondTemplate(film){
+    return `<div class="film">
+         <b>Film:</b> ${film.Film} <br />
+         <b>Title:</b> ${film.Title}  <br />
+         <b>Year:</b>${film.Year}  <br />
+         <b>Director:</b> ${film.Director}  <br />
+         <b>Producers:</b> ${film.Producers} <br />
+         <b>Writers:</b> ${film.Writers}  <br />
+         <b>Composer:</b> ${film.Composer}  <br />
+         <b>Budget:</b>  ${film.Budget} <br />
+         <b>BoxOffice:</b> ${film.BoxOffice}  <br />
+    <div class="pic"><img src="thumbnails/${film.Image}"></div>
+
+  </div>`;
+  }
 $(document).ready(function() {  
-	$('.category').click(function(e){
+
+    $('.category').click(function(e){
         e.preventDefault(); //stop default action of the link
-		cat = $(this).attr("href");  //get category from URL
-  	var request = $.ajax({
-      url: "api.php?cat=" + cat,
-      method: "GET",
-      dataType: "json"
+cat = $(this).attr("href");  //get category from URL
+
+      var request = $.ajax({
+        url: "api.php?cat=" + cat,
+        method: "GET",
+        dataType: "json"
+    });
+    request.done(function( data ) {
+        console.log(data);
+      //Place the title of the webservice of the page
+     
+      $("#filmtitle").html(data.title);
+      $("#films").html("");
+
+     
+      $.each(data.films,function(key,value){
+        let str = bondTemplate(value);
+        $("<div></div>").html(str).appendTo("#films");
       });
-      request.done(function( data ) {
-       console.log(data);
-        // place the title on the page
-        $("#epltitle").html(data.title);
-        // clears the previous films
-        $("#eplteams").html("");
-        
-        // loops through films and adds to page
-        $.each(data.teams, function(key,value){
-          let str = eplTemplate(value); // is the array
-          $("<div></div>").html(str).appendTo("#nbateams");
-          
-        });
-        //view JSON as a string
-        /*
-        let myData = JSON.stringify(data, null, 4);
-        myData = "<pre>" + myData + "</pre>";
-        $("#output").html(myData);
-        */
-      });
-      request.fail(function(xhr, status, error) {
+     //  $("#output").text(JSON.stringify(data));
+
+      let myData = JSON.stringify(data,null,4);
+   myData = "<pre>" + myData + "</pre>";
+      $("#output").html(myData);
+    });
+        request.fail(function(xhr, status, error) {
                //Ajax request failed.
                var errorMessage = xhr.status + ': ' + xhr.statusText
                alert('Error - ' + errorMessage);
-           }
-    );
-	});
-});	
+           
+        });
+       
+        });
+        });
+
 </script>
 </head>
-	<body>
-	<h1>EPL Teams Web Service</h1>
-		<a href="year" class="category">EPL Teams</a><br />
-		<a href="box" class="category">EPL Teams By Championships</a>
-		<h3 id="epltitle">Title Will Go Here</h3>
-		<div id="eplteams">
-			<p>EPL Teams will go here</p>
-		</div>
-    <!-- <div class="film">
-      <b>Film: <b> 1<br />
-      <b>Title: <b> Dr. No<br />
-      <b>Year: <b> 1962<br />
-      <b>Director: <b> Terence Young<br />
-      <b>Producers: <b> Harry Saltzman and Albert R. Broccoli<br />
-      <b>Writers: <b> Richard Maibaum, Johanna Harwood and Berkely Mather<br />
-      <b>Composer: <b> Monty Norman<br />
-      <b>Bond: <b> Sean Connery<br />
-      <b>Budget: <b> $1,000,000.00<br />
-      <b>BoxOffice: <b> $59,567,035.00<br />
-      <div class="pic"><img src="thumbnails/dr-no.jpg"/></div>
-    </div> -->
-		<div id="output">Results go here</div>
-	</body>
+<body>
+    <h1>My Top Ten Movies Of All Time</h1>
+    <a href="year" class="category">My Top Ten Movies Of All Time</a><br />
+  <a href="box" class="category">My Top Ten Movies Of All Time By International Box Office Totals</a>
+    <h3 id="filmtitle"></h3>
+    <div id="films">
+    <p>Films will go here</p>
+     </div>
+
+<div id="output"></div>
+</body>
 </html>
